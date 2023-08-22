64c9aa8c9a25d84635ad260be9956d779ec75c67
heyyouhere <heyyouhere@hyh.com>
2023-08-22 17:39:51 +0300

    Init

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1837590.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 146341737, 146341738, 146341768, 146341744, 146341745, 146341747, 146341751, 146341752, 146341754, 146341755, 146341756, 146341757, 146341759, 146341760, 146443430 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/146341765/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/146341766/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/146341764/1/ammo.js', 'preload' : true},
+];
